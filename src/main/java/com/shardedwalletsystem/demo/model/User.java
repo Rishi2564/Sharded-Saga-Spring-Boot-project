d0a1cf8aecbf4319a1c36c2cedc6bd45e55a1bfe
Rishi2564 <rishabhpradhan2005@gmail.com>
2026-07-20 00:58:32 +0530

    Complete initial project setup and database configuration

--- src/main/java/com/shardedwalletsystem/demo/model/User.java
+++ src/main/java/com/shardedwalletsystem/demo/model/User.java
@@ -1,4 +1,26 @@
 package com.shardedwalletsystem.demo.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
 }
